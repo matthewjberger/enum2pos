29a10a21dd2e039baedec49d897e6429b4e1b69b
Matthew J. Berger <matt@usehyphen.com>
2023-03-24 14:12:36 -0600

    cleanup test names

--- tests/convert.rs
+++ tests/convert.rs
@@ -1,16 +1,13 @@
 use enum2pos::EnumIndex;
 
-// Example enum to test the macro
-#[derive(EnumIndex, Debug, PartialEq)]
+#[derive(EnumIndex, PartialEq, Debug)]
 enum SampleEnum {
     Unit,
     Unnamed(i32, String),
-    Named { field: u32 },
 }
 
-// Test for to_index function
 #[test]
-fn test_to_index() {
+fn to_index() {
     let unit = SampleEnum::Unit;
     let unnamed = SampleEnum::Unnamed(42, String::from("test"));
 
@@ -18,9 +15,8 @@ fn test_to_index() {
     assert_eq!(unnamed.to_index(), 1);
 }
 
-// Test for from_index function with unit variant
 #[test]
-fn test_from_index_unit() {
+fn from_index_unit() {
     let index = 0;
     let args: Vec<String> = vec![];
     let expected = Some(SampleEnum::Unit);
@@ -28,9 +24,8 @@ fn test_from_index_unit() {
     assert_eq!(SampleEnum::from_index(index, args), expected);
 }
 
-// Test for from_index function with unnamed variant
 #[test]
-fn test_from_index_unnamed() {
+fn from_index_unnamed() {
     let index = 1;
     let args = vec!["42".to_string(), "test".to_string()];
     let expected = Some(SampleEnum::Unnamed(42, String::from("test")));
@@ -38,9 +33,8 @@ fn test_from_index_unnamed() {
     assert_eq!(SampleEnum::from_index(index, args), expected);
 }
 
-// Test for from_index function with invalid index
 #[test]
-fn test_from_index_invalid() {
+fn from_index_invalid() {
     let index = 2;
     let args: Vec<String> = vec![];
 
